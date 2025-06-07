96340497b9a6516976aed8f28e4d0c1ec3612db5
chriskoup <mai25028@uom.edu.gr>
2025-06-07 17:40:44 +0300

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+import Web3 from "web3";
 import Home from "./Home";
 
 function App() {
-  return <Home />;
+  const [account, setAccount] = useState("");
+
+  useEffect(() => {
+    const loadAccount = async () => {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+          setAccount(accounts[0]);
+        } catch (error) {
+          console.error("User denied account access");
+        }
+      }
+    };
+    loadAccount();
+  }, []);
+
+  return <Home account={account} />;
 }
 
 export default App;
