1bff76150c1947a9b2ba9b0b06688991f4715014
chriskoup <mai25028@uom.edu.gr>
2025-06-07 12:07:34 +0300

    Update eslint.config.mjs

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -3,8 +3,19 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["eslint:recommended"], languageOptions: { globals: {globals.browser, BigInt: "readonly"} } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        BigInt: "readonly"
+      }
+    },
+    plugins: {
+      js
+    },
+    extends: ["eslint:recommended"],
+  },
+  pluginReact.configs.flat.recommended
 ]);
